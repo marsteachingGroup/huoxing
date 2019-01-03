9763071635f58d6b18717568ec3f533afddf53d5
zhaoxiaocong <zhaoxiaocong@mofly.cn>
2019-01-03 19:10:41 +0800

    fix(signup): 增加表单样式

--- signup/src/pages/basic.vue
+++ signup/src/pages/basic.vue
@@ -8,12 +8,68 @@
 
 		<div class="config-content">
 
+			<div class="config-content_item jfk-flex">
+				<div class="config-content_left">
+				  <img class="config-content_icon" src="../assets/images/icon/1.png" alt="">
+				</div>
+				<span class="config-content_word">参赛身份</span>
+				<div class="config-content_radio">
+					<label class="config-content_label">
+						<div class="config-content_input">
+							<span class="config-content_inner" :class="{'config-content_active': member === '1'}"></span>
+						</div>
+						<input v-model="member" type="radio" value="1"/><span>队长</span>
+					</label> 
+					<label class="config-content_label">
+						<div class="config-content_input">
+						  <span class="config-content_inner" :class="{'config-content_active': member === '2'}"></span>
+						</div>
+						<input v-model="member" type="radio" value="2" /><span>队员</span>
+					</label> 
+				</div>
+			</div>
+
+			<div class="config-content_item jfk-flex">
+				<div class="config-content_left">
+				  <img class="config-content_icon" src="../assets/images/icon/2.png" alt="">
+				</div>
+				<span class="config-content_word">个人手机</span>
+				<div class="config-content_content">
+					<input type="text" class="config-content_text">
+				</div>
+			</div>
+
+			<div class="config-content_item jfk-flex">
+				<div class="config-content_left">
+				  <img class="config-content_icon" src="../assets/images/icon/3.png" alt="">
+				</div>
+				<span class="config-content_word">验证码</span>
+				<div class="config-content_content">
+					<input type="text" class="config-content_text">
+				</div>
+				<span class="config-content_qrcode">获取验证码</span>
+			</div>
+
+			<div class="config-content_item jfk-flex">
+				<div class="config-content_left">
+				  <img class="config-content_icon" src="../assets/images/icon/4.png" alt="">
+				</div>
+				<span class="config-content_word">常用邮箱</span>
+				<div class="config-content_content">
+					<input type="text" class="config-content_text">
+				</div>
+			</div>
 		</div>
   </div>
 </template>
 
 <script>
 export default {
+	data () {
+	  return {
+		member: '1'
+	  }
+	}
 }
 </script>
 <style lang="scss">

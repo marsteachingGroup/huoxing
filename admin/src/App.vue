b61d8590287ebebf186bfbc2a3fed2262ac9653c
zhaoxiaocong <zhaoxiaocong@mofly.cn>
2019-02-01 18:32:21 +0800

    feat(admin): 初始化项目

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -1,9 +1,14 @@
 <template>
-  <div id='app'>
-  <router-view></router-view>
+  <div id="app">
+    <router-view/>
   </div>
 </template>
+
 <script>
+export default {
+  name: 'App'
+}
 </script>
+
 <style>
 </style>
--- admin/src/main.js
+++ admin/src/main.js
@@ -1,15 +1,8 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import axios from './api/axio_config.js'
-import {dateFormat} from './common/utils.js'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
-import 'font-awesome/css/font-awesome.min.css'
-Vue.prototype.$http = axios
-Vue.prototype.$dateFormat = dateFormat
-
-Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
@@ -17,6 +10,6 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  template: '<App/>',
-  components: { App }
+  components: { App },
+  template: '<App/>'
 })
